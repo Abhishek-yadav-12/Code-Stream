2908e4c32f5102fece93f4711efbca0191333477
Abhishek-yadav-12 <abhishekay2003@gmail.com>
2025-03-12 23:29:43 +0530

    Editor fix

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -98,7 +98,7 @@ const EditorPage = () => {
                     <div className="logo">
                         <img
                             className="logoImage"
-                            src="/code-sync.png"
+                            src="/1.png"
                             alt="logo"
                         />
                     </div>
